6154591d529419308e7feb1dbd6f1184c0690c2b
616games <86506015+616games@users.noreply.github.com>
2021-07-04 04:54:48 -0400

    Created Delegates script

---
+++ Assets/Scripts/Delegates.cs
@@ -0,0 +1,4 @@
+/// <summary>
+/// Used to allow us to return a method that identifies the type of function to be graphed.
+/// </summary>
+public delegate float GraphFunction(float _xPos, float _zPos, float _time);
