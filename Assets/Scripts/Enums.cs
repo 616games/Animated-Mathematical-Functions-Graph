fe2837a2983a484167b1df23ec20ac3b4cab20ca
616games <86506015+616games@users.noreply.github.com>
2021-07-05 02:53:14 -0400

    Updated Enums script
    Removed the Dimension enum as it is not relevant for this example.

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -2,8 +2,3 @@
 /// Describes the type of function to be graphed.
 /// </summary>
 public enum GraphFunctionType { Line, Squared, Cubed, Wave, MultiWave, MultiWave2, Ripple }
-
-/// <summary>
-/// Describes the dimension in which the function should be graphed.
-/// </summary>
-public enum Dimension { Two, Three }
